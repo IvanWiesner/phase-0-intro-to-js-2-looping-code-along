4eafc11f982d1a92d54c55e61da40b54c52f1794
Ivan <ivanwiesner@gmail.com>
2021-04-17 10:06:18 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+    const textArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+        textArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return textArray;
+}
+function countDown(x) {
+    let number = x;
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
